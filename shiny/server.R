89afb597038ebf49851b7fa6a6d98e179decd775
RotarB333 <bor.rotar1@gmail.com>
2018-02-11 17:23:05 +0100

    zboljšan shiny

--- shiny/server.R
+++ shiny/server.R
@@ -4,83 +4,48 @@ library(datasets)
 shinyServer(function(input, output) {
   output$box <- renderPlot({
     
-    if(input$type == "HDIrank SVET"){
-      print(graf.hdirank)}
-    else if(input$type == "HDIrank Afrika"){
-      print(graf.hdirank.afrika)}
-    else if(input$type == "HDIrank Ameriki"){
-      print(graf.hdirank.amerika)}
-    else if(input$type == "HDIrank Azija"){
-      print(graf.hdirank.azija)}
-    else if(input$type == "HDIrank Evropa"){
-      print(graf.hdirank.evropa)}
-    else if(input$type == "HDIrank Oceanija"){
-      print(graf.hdirank.oceanija)}
-    
-    else if(input$type == "Pričakovana starost SVET"){
-      print(graf.starost)}
-    else if(input$type == "Pričakovana starost Afrika"){
-      print(graf.starost.afrika)}
-    else if(input$type == "Pričakovana starost Ameriki"){
-      print(graf.starost.amerika)}
-    else if(input$type == "Pričakovana starost Azija"){
-      print(graf.starost.azija)}
-    else if(input$type == "Pričakovana starost Evropa"){
-      print(graf.starost.evropa)}
-    else if(input$type == "Pričakovana starost Oceanija"){
-      print(graf.starost.oceanija)}
-    
-    else if(input$type == "mrate SVET"){
-      print(graf.mrate)}
-    else if (input$type == "mrate Afrika"){
-      print(graf.mrate.afrika)}
-    else if (input$type == "mrate Ameriki"){
-      print(graf.mrate.amerika)}
-    else if (input$type == "mrate Azija"){
-      print(graf.mrate.azija)}
-    else if (input$type == "mrate Evropa"){
-      print(graf.mrate.evropa)}
-    else if (input$type == "mrate Oceanija"){
-      print(graf.mrate.oceanija)}
-    
-    else if (input$type == "Pričakovana dolžina šolanja SVET"){
-      print(graf.psola)}
-    else if (input$type == "Pričakovana dolžina šolanja Afrika"){
-      print(graf.psola.afrika)}
-    else if (input$type == "Pričakovana dolžina šolanja Ameriki"){
-      print(graf.psola.amerika)}
-    else if (input$type == "Pričakovana dolžina šolanja Azija"){
-      print(graf.psola.azija)}
-    else if (input$type == "Pričakovana dolžina šolanja Evropa"){
-      print(graf.psola.evropa)}
-    else if (input$type == "Pričakovana dolžina šolanja Oceanija"){
-      print(graf.psola.oceanija)}
-    
-    else if (input$type == "Dejanska dolžina šolanja SVET"){
-      print(graf.dsola)}
-    else if (input$type == "Dejanska dolžina šolanja Afrika"){
-      print(graf.dsola.afrika)}
-    else if (input$type == "Dejanska dolžina šolanja Ameriki"){
-      print(graf.dsola.amerika)}
-    else if (input$type == "Dejanska dolžina šolanja Azija"){
-      print(graf.dsola.azija)}
-    else if (input$type == "Dejanska dolžina šolanja Evropa"){
-      print(graf.dsola.evropa)}
-    else if (input$type == "Dejanska dolžina šolanja Oceanija"){
-      print(graf.dsola.oceanija)}
-    
-    else if (input$type == "GNI na prebivalca SVET"){
-      print(graf.gni)}
-    else if (input$type == "GNI na prebivalca Afrika"){
-      print(graf.gni.afrika)}
-    else if (input$type == "GNI na prebivalca Ameriki"){
-      print(graf.gni.amerika)}
-    else if (input$type == "GNI na prebivalca Azija"){
-      print(graf.gni.azija)}
-    else if (input$type == "GNI na prebivalca Evropa"){
-      print(graf.gni.evropa)}
-    else if (input$type == "GNI na prebivalca Oceanija"){
-      print(graf.gni.oceanija)}
+    if(input$type1 == "HDIrank"){
+      if(input$type2 == "SVET"){print(graf.hdirank)}
+      else if(input$type2 == "Afrika"){print(graf.hdirank.afrika)}
+      else if(input$type2 == "Ameriki"){print(graf.hdirank.amerika)}   
+      else if(input$type2 == "Azija"){print(graf.hdirank.azija)}
+      else if(input$type2 == "Evropa"){print(graf.hdirank.evropa)}
+      else if(input$type2 == "Oceanija"){print(graf.hdirank.oceanija)}}
+    else if(input$type1 == "Pričakovana starost"){
+      if(input$type2 == "SVET"){print(graf.starost)}
+      else if(input$type2 == "Afrika"){print(graf.starost.afrika)}
+      else if(input$type2 == "Ameriki"){print(graf.starost.amerika)}   
+      else if(input$type2 == "Azija"){print(graf.starost.azija)}
+      else if(input$type2 == "Evropa"){print(graf.starost.evropa)}
+      else if(input$type2 == "Oceanija"){print(graf.starost.oceanija)}}
+    else if(input$type1 == "mrate"){
+      if(input$type2 == "SVET"){print(graf.mrate)}
+      else if(input$type2 == "Afrika"){print(graf.mrate.afrika)}
+      else if(input$type2 == "Ameriki"){print(graf.mrate.amerika)}   
+      else if(input$type2 == "Azija"){print(graf.mrate.azija)}
+      else if(input$type2 == "Evropa"){print(graf.mrate.evropa)}
+      else if(input$type2 == "Oceanija"){print(graf.mrate.oceanija)}}
+    else if(input$type1 == "Pričakovana dolžina šolanja"){
+      if(input$type2 == "SVET"){print(graf.psola)}
+      else if(input$type2 == "Afrika"){print(graf.psola.afrika)}
+      else if(input$type2 == "Ameriki"){print(graf.psola.amerika)}   
+      else if(input$type2 == "Azija"){print(graf.psola.azija)}
+      else if(input$type2 == "Evropa"){print(graf.psola.evropa)}
+      else if(input$type2 == "Oceanija"){print(graf.psola.oceanija)}}
+    else if(input$type1 == "Dejanska dolžina šolanja"){
+      if(input$type2 == "SVET"){print(graf.psola)}
+      else if(input$type2 == "Afrika"){print(graf.psola.afrika)}
+      else if(input$type2 == "Ameriki"){print(graf.psola.amerika)}   
+      else if(input$type2 == "Azija"){print(graf.psola.azija)}
+      else if(input$type2 == "Evropa"){print(graf.psola.evropa)}
+      else if(input$type2 == "Oceanija"){print(graf.psola.oceanija)}}
+    else if(input$type1 == "GNI na prebivalca"){
+      if(input$type2 == "SVET"){print(graf.gni)}
+      else if(input$type2 == "Afrika"){print(graf.gni.afrika)}
+      else if(input$type2 == "Ameriki"){print(graf.gni.amerika)}   
+      else if(input$type2 == "Azija"){print(graf.gni.azija)}
+      else if(input$type2 == "Evropa"){print(graf.gni.evropa)}
+      else if(input$type2 == "Oceanija"){print(graf.gni.oceanija)}}
     
   })
 }
--- shiny/ui.R
+++ shiny/ui.R
@@ -4,18 +4,10 @@ shinyUI(fluidPage(
   titlePanel("Geografska širina in inidkatorji razvoja"), 
   sidebarLayout(
     sidebarPanel(
-      selectInput("type",label="Indikator - regija",
-                  choice=c("HDIrank SVET","HDIrank Afrika","HDIrank Ameriki","HDIrank Azija","HDIrank Evropa","HDIrank Oceanija",
-                           "Pričakovana starost SVET","Pričakovana starost Afrika","Pričakovana starost Ameriki","Pričakovana starost Azija",
-                           "Pričakovana starost Evropa","Pričakovana starost Oceanija",
-                           "mrate SVET","mrate Afrika","mrate Ameriki","mrate Azija","mrate Evropa","mrate Oceanija",
-                           "Pričakovana dolžina šolanja SVET","Pričakovana dolžina šolanja Afrika","Pričakovana dolžina šolanja Ameriki",
-                           "Pričakovana dolžina šolanja Azija","Pričakovana dolžina šolanja Evropa","Pričakovana dolžina šolanja Oceanija",
-                           "Dejanska dolžina šolanja SVET","Dejanska dolžina šolanja Afrika","Dejanska dolžina šolanja Ameriki",
-                           "Dejanska dolžina šolanja Azija","Dejanska dolžina šolanja Evropa","Dejanska dolžina šolanja Oceanija",
-                           "GNI na prebivalca SVET","GNI na prebivalca Afrika","GNI na prebivalca Ameriki","GNI na prebivalca Azija",
-                           "GNI na prebivalca Evropa","GNI na prebivalca Oceanija")
-      )
+      selectInput("type1",label="Indikator",
+                  choice=c("HDIrank","Pričakovana starost","mrate","Pričakovana dolžina šolanja",
+                           "Dejanska dolžina šolanja","GNI na prebivalca")),
+      selectInput("type2",label="regija",choice=c("SVET","Afrika","Ameriki","Azija","Evropa","Oceanija"))
     ),
     mainPanel(plotOutput("box")
     ) 
